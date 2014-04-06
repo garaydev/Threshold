1f319fe805202a545db8a04f2bea7f2ac215a69f
Angel G. <garaydev@gmail.com>
2014-04-06 03:21:44 -0400

    Third Change
    changed

--- js/threadhold.js
+++ js/threadhold.js
@@ -1,4 +1,26 @@
+//checks to see if the document is ready
+$(document).ready(function() {
 
+
+document.getElementById('submit').onclick = function() {
+	 isformfilled = true;
+
+	 term1 = $("#term1").val(); 
+	 term2 = $("#term2").val(); 
+	 term3 = $("#term3").val(); 
+	 term4 = $("#term4").val();
+	 $('input[type="text"]').each(function() {
+             if ($.trim($(this).val()) == '') {
+                 isformfilled = false;
+               }
+               });
+	 if (isformfilled == false)
+	 {
+	 	window.alert("Not all fields were filled out!");
+	 }
+	 
+
+}
 function loadXMLDoc(filename)
 {
 if (window.XMLHttpRequest)
@@ -9,7 +31,9 @@ else // code for IE5 and IE6
   {
   xhttp=new ActiveXObject("Microsoft.XMLHTTP");
   }
-xhttp.open("GET",filename,false);
+xhttp.open("GET","C:\inetpub\wwwroot\Threshold\\" + filename,false);
 xhttp.send();
 return xhttp.responseXML;
 } 
+
+});
